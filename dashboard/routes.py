0a023dc0352120cf9ee91e98cfcac73d3a292a03
Jasen Lo <jasenlo123@gmail.com>
2021-03-02 19:22:05 -0800

    CRUD posts

--- dashboard/routes.py
+++ dashboard/routes.py
@@ -1,31 +1,14 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from dashboard import app, db, bcrypt
-from dashboard.forms import LoginForm
+from dashboard.forms import LoginForm, PostForm
 from dashboard.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-
-posts = [
-    {
-        'author': 'Konti Tester',
-        'title': 'Blog Post 1',
-        'content': 'post content',
-        'date_posted': "yesterday"
-    },
-    {
-        'author': 'Konti Tester 2',
-        'title': 'Blog Post 2',
-        'content': 'more content',
-        'date_posted': "yesterday"
-
-    }
-]
-
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Welcome')
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -37,6 +20,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
+            flash(f'Hi {user.name}, you have been logged in.', 'success')
             return redirect(url_for('dashboard'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
@@ -45,9 +29,61 @@ def login():
 @app.route("/dashboard")
 @login_required
 def dashboard():
+    posts = Post.query.all()
     return render_template('dashboard.html',posts= posts)
 
 @app.route("/logout")
 def logout():
     logout_user()
+    flash('You have been logged out.', 'success')
     return redirect(url_for('home'))
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('new_post.html', title='New Post',
+                           form=form, legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('new_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('dashboard'))
